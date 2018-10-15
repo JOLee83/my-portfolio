a42a8d9067896bc46357e8aeda35b19595f689d5
JOLee83 <lee.justin.oliver@gmail.com>
2018-10-15 14:29:35 -0400

    added google doc for resume

--- src/Components/Content/Resume.js
+++ src/Components/Content/Resume.js
@@ -3,20 +3,33 @@ import styled from 'styled-components'
 import resume from '../../Resources/Justin_Oliver_Lee_Resume.pdf'
 
 const Document = styled.div`
-	width: 80%;
+	width: 100%;
 	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	img {
+		border: 1px solid black;
+		width: 75%;
+		margin: 10px;
+	}
+	@media (min-width: 1000px) {
+		width: 50%;
+		img {
+			width: 100%;
+		}
+	}
 `
 class Resume extends Component {
 	render() {
 		return (
 			<Document>
-				{/* <iframe
-					title="resume"
-					src="./Justin_Oliver_Lee_Resume.pdf"
-					style={{ width: '100%', height: '100%' }}
-				/> */}
-				<a href={resume} target="_blank">
-					Resume (PDF)
+				<img alt="resume" src="./Justin_Oliver_Lee_Resume.jpg" />
+				<a
+					href="https://drive.google.com/file/d/1TuEUxaJHyiaz4SVyjLEzUuuqmxlJBvIq/view?usp=sharing"
+					target="_blank"
+				>
+					<i className="fas fa-file" /> Resume (PDF)
 				</a>
 			</Document>
 		)
